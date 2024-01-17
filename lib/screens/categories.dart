b1cc502b510306338a2ed474550597d90b92f005
TrungPham <trungphamdac201@gmail.com>
2024-01-18 00:50:15 +0700

    Finshed app

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,21 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
 import 'package:meals/models/category.dart';
+import 'package:meals/models/meal.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({super.key});
+  const CategoriesScreen({
+    super.key,
+    required this.onToggleFavorite,
+    required this.availableMeals,
+  });
+
+  final void Function(Meal meal) onToggleFavorite;
+  final List<Meal> availableMeals;
 
   void _selectCategory(BuildContext context, Category category) {
-    final filterMeals = dummyMeals
-        .where((meal) => meal.categories.contains(category.id))
-        .toList();
-    Navigator.push(
-        context,
-        MaterialPageRoute(
-            builder: (ctx) =>
-                MealsScreen(title: category.title, meals: filterMeals)));
+    final filterMeals = availableMeals.where((meal) => meal.categories.contains(category.id)).toList();
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filterMeals,
+          onToggleFavorite: onToggleFavorite,
+        ),
+      ),
+    );
   }
 
   @override
